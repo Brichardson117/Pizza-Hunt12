f4f9ec45db1856009e5809c567f596443cef0bbe
Brittney <brittneyrichardson8@gmail.com>
2022-06-26 11:47:18 -0500

    added offline persistence

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -24,7 +24,6 @@ const pizzaController = {
         path: 'comments',
         select: '-__v'
       })
-      .select('-__v')
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
